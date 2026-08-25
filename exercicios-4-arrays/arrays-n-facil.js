068e6e9bf37f18872766e75614440d9c7c018c28
João Vitor <joaovitorfesouza@gmail.com>
2026-08-25 10:15:15 -0300

    Feito exercicios de arrays do 1 ao 7

---
+++ exercicios-4-arrays/arrays-n-facil.js
@@ -0,0 +1,63 @@
+/* EXERCICIO 1*/
+
+// const frutas = ["banana", "laranja", "maça", "abacate", "limao"]
+
+// console.log(frutas[0]);
+// console.log(frutas[frutas.length - 1]);
+
+// EXERCICIO 2
+
+// const numeros = [1, 2, 3, 4, 5]
+
+// console.log(numeros[0]);
+// console.log(numeros[2]);
+// console.log(numeros[4]);
+
+// EXERCICIO 3 e 4
+
+// const transportes = ["onibus", "carro", "moto"]
+
+// transportes[1] = "caminhao"
+
+// transportes.length
+
+// console.log(transportes);
+
+// EXERCICIO 5 
+
+// const numeros = [5, 10, 15, 20, 25]
+
+// for(let i = 0; i < numeros.length; i++) {
+// console.log(numeros[i]);
+
+// }
+
+// EXERCICIO 6
+
+// const numeros = [5, 10, 15, 20, 25]
+
+// let soma = 0
+
+// for(let i = 0; i < numeros.length; i++) {
+//   soma += numeros[i]
+
+// }
+// console.log(soma)
+
+//EXERCICIO 7 
+
+const numeros = [8, 15, 3, 40, 21];
+
+let maior = 0
+
+for (let i = 0; i < numeros; i++) {
+  
+  if(numeros[i] > maior){
+    maior = numeros[i]
+  }
+
+};
+
+
+
+
