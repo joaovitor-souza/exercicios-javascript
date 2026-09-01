6d070ab91bd0c34917afedaab60c1a600f072a69
João Vitor <joaovitorfesouza@gmail.com>
2026-09-01 05:43:27 -0300

    Correção feita no exercicio 7

--- exercicios-4-arrays/arrays-n-facil.js
+++ exercicios-4-arrays/arrays-n-facil.js
@@ -50,13 +50,16 @@ const numeros = [8, 15, 3, 40, 21];
 
 let maior = 0
 
-for (let i = 0; i < numeros; i++) {
+for (let i = 0; i < numeros.length; i++) {
   
   if(numeros[i] > maior){
     maior = numeros[i]
   }
-
+  
 };
+
+console.log(maior);
+
 
 
 
